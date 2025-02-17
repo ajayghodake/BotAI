7f5fa9d6dfebcf41de265334541d622a2d94a141
Ajay Ghodake <it.ajayghodake@gmail.com>
2025-02-18 01:27:38 +0530

    code updated

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -65,7 +65,7 @@ const Home = () => {
     >
       <Navbar />
 
-      {chat.length == 0 && <InitChat generateResponse={generateResponse} />}
+      {chat.length === 0 && <InitChat generateResponse={generateResponse} />}
 
       {chat.length > 0 && (
         <Stack
